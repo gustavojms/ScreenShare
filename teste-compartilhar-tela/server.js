54252ce4eb563025d4e135dd18561e0e04c1ef02
Nasc1mento <reisadryan16@gmail.com>
2023-04-03 16:43:54 -0300

    pasta teste-compartilhar-tela

---
+++ teste-compartilhar-tela/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*'
+  }
+});
+
+app.use(express.static('public'));
+
+app.get('/server', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/client', (req, res) => {
+    res.sendFile(__dirname + '/public/client.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer', answer);
+  });
+
+  socket.on('candidate', (candidate) => {
+    socket.broadcast.emit('candidate', candidate);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
