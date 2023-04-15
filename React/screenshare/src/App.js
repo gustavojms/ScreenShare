3cdb42818ee9562a50b2d0a67aa9e911f06a1b51
antonio87 <antonioppedro87@gmail.com>
2023-04-14 22:40:28 -0300

    subindo projeto react

---
+++ React/screenshare/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home'
+import Client from './Pages/Client'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Client" element={<Client/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ React/screenshare/src/Pages/Client.js
@@ -0,0 +1,9 @@
+function Client(){
+    return(
+        <div>
+            <h1>teste tela client</h1>
+        </div>
+    )
+}
+
+export default Client;
