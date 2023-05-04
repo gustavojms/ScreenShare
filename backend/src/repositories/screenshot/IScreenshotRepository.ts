2c483a9a60ed2e88f0a575d1932c0df899a129e9
andreymafra55 <andreymafra55@gmail.com>
2023-05-04 18:54:03 -0300

    comentando e apagando parte do codigo para nao quebrar o aplicacao

--- backend/src/repositories/screenshot/IScreenshotRepository.ts
+++ backend/src/repositories/screenshot/IScreenshotRepository.ts
@@ -1,15 +1,15 @@
-export interface CreateSrecreenshotData {
-  id: number;
-  screenshot: Blob;
-  userId: number;
-}
-export interface CreateSrecreenshot {
-  screenshot: Blob;
-  userId: number;
-}
+// export interface CreateSrecreenshotData {
+//   id: number;
+//   screenshot: Blob;
+//   userId: number;
+// }
+// export interface CreateSrecreenshot {
+//   screenshot: Blob;
+//   userId: number;
+// }
 
-export interface ScreenshotRepository {
-  create(data: CreateSrecreenshot): Promise<CreateSrecreenshot>;
-  findById(id: number): Promise<CreateSrecreenshotData | null>;
-  delete(id: number): Promise<CreateSrecreenshotData>;
-}
+// export interface ScreenshotRepository {
+//   create(data: CreateSrecreenshot): Promise<CreateSrecreenshot>;
+//   findById(id: number): Promise<CreateSrecreenshotData | null>;
+//   delete(id: number): Promise<CreateSrecreenshotData>;
+// }
--- backend/src/services/screenshot/deleteScreenshotService.ts
+++ backend/src/services/screenshot/deleteScreenshotService.ts
@@ -1,16 +1,16 @@
-import {
-  ScreenshotRepository,
-  CreateSrecreenshotData,
-} from "../../repositories/screenshot/IScreenshotRepository";
+// import {
+//   ScreenshotRepository,
+//   CreateSrecreenshotData,
+// } from "../../repositories/screenshot/IScreenshotRepository";
 
-export class DeleteScreenshotService {
-  constructor(private screenshotRepository: ScreenshotRepository) {}
+// export class DeleteScreenshotService {
+//   constructor(private screenshotRepository: ScreenshotRepository) {}
 
-  async execute(
-    data: CreateSrecreenshotData["id"]
-  ): Promise<CreateSrecreenshotData> {
-    const screenshot = await this.screenshotRepository.delete(data);
+//   async execute(
+//     data: CreateSrecreenshotData["id"]
+//   ): Promise<CreateSrecreenshotData> {
+//     const screenshot = await this.screenshotRepository.delete(data);
 
-    return screenshot;
-  }
-}
+//     return screenshot;
+//   }
+// }
