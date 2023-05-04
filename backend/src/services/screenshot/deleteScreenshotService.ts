6f79ee11f09c272fd877c86a5b858564ba9aab22
andreymafra55 <andreymafra55@gmail.com>
2023-05-04 12:49:39 -0300

    criacao dos screenshots services

---
+++ backend/src/services/screenshot/deleteScreenshotService.ts
@@ -0,0 +1,16 @@
+import {
+  ScreenshotRepository,
+  CreateSrecreenshotData,
+} from "../../repositories/screenshot/IScreenshotRepository";
+
+export class DeleteScreenshotService {
+  constructor(private screenshotRepository: ScreenshotRepository) {}
+
+  async execute(
+    data: CreateSrecreenshotData["id"]
+  ): Promise<CreateSrecreenshotData> {
+    const screenshot = await this.screenshotRepository.delete(data);
+
+    return screenshot;
+  }
+}
