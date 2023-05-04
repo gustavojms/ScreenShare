b065e1d5df49e63bd104a59640209a3ca7b6e6e5
andreymafra55 <andreymafra55@gmail.com>
2023-05-04 12:49:23 -0300

    criacao dos screenshots repositories

---
+++ backend/src/repositories/screenshot/PrismaScreenshotRepository.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../../database/prismaClient";
+import {
+  CreateSrecreenshotData,
+  CreateSrecreenshot,
+  ScreenshotRepository,
+} from "./IScreenshotRepository";
+
+export class PrismaSreenshotRepository implements ScreenshotRepository {
+  async create(data: CreateSrecreenshotData): Promise<CreateSrecreenshot> {
+    const screenshot = await prisma.screenshot.create({
+      data: {
+        screenshot: data.screenshot,
+      },
+    });
+    return screenshot;
+  }
+
+  async findById(id: number): Promise<CreateSrecreenshotData | null> {
+    const screenshot = await prisma.screenshot.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return screenshot;
+  }
+
+  async delete(id: number): Promise<CreateSrecreenshotData> {
+    const screenshot = await prisma.screenshot.delete({
+      where: {
+        id: id,
+      },
+    });
+    return screenshot;
+  }
+}
