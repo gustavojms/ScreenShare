92e1f39d26d9451bf40428ad5a12a73d10513694
nucasoares <nubiacatarina50@gmail.com>
2023-05-04 21:53:25 -0300

    edicao do registro

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -8,8 +8,8 @@ import Button from "../Components/Button";
 interface FormValues {
     nome: string;
     email: string;
-    matricula: string;
     senha: string;
+    confirmarSenha: string;
 }
 
 const Register: FC = () => {
@@ -17,8 +17,8 @@ const Register: FC = () => {
     const [formValues, setFormValues] = useState<FormValues>({
         nome: "",
         email: "",
-        matricula: "",
         senha: "",
+        confirmarSenha: "",
     });
 
     const handleInputChange = (
@@ -33,11 +33,14 @@ const Register: FC = () => {
         if (
             !formValues.nome ||
             !formValues.email ||
-            !formValues.matricula ||
-            !formValues.senha
-
+            !formValues.senha ||
+            !formValues.confirmarSenha
         ) {
             alert("Preencha todos os campos!");
+            return;
+        }
+        if (formValues.senha !== formValues.confirmarSenha){
+            alert("As senhas não coincidem!");
             return;
         }
         console.log(formValues);
@@ -46,7 +49,7 @@ const Register: FC = () => {
     return (
         <body className="items-center justify-center flex h-screen">
             <div className="shadow-lg items-center justify-center p-2 bg-purple-400 w-96 rounded">
-                <h1 className="text-center text-3xl text-white font-semibold">Register</h1>
+                <h1 className="text-center text-3xl text-white font-semibold">Criar Conta</h1>
                 <form onSubmit={handleSubmit}>
                     <TextField
                         label="Nome"
@@ -61,19 +64,20 @@ const Register: FC = () => {
                         value={formValues.email}
                         onChange={handleInputChange} />
                     <TextField
-                        label="Matricula"
-                        placeholder="Matricula"
-                        name="matricula"
-                        value={formValues.matricula}
-                        onChange={handleInputChange}
-                        />
-                    <TextField
                         label="Senha"
                         placeholder="Senha"
                         name="senha"
                         value={formValues.senha}
-                        onChange={handleInputChange}/>    
-                    <Button button="Register"/>
+                        type="password"
+                        onChange={handleInputChange}/> 
+                    <TextField
+                        label="Confirmar Senha"
+                        placeholder="Confirmar Senha"
+                        name="confirmarSenha"
+                        value={formValues.confirmarSenha}
+                        type="password"
+                        onChange={handleInputChange}/>   
+                    <Button button="Criar Conta"/>
                 </form>
                 <p className="text-white text-center font-semibold">Já possui conta? Entre clicando <Link to={"/Login"} className="text-blue-700 hover:text-blue-800">aqui</Link></p>
             </div>
