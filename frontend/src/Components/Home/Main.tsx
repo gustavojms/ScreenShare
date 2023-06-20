5481a988f956f2ab742a9d9b8aaf5d0076fdb816
afreis <reisadryan16@gmail.com>
2023-06-20 15:17:11 -0300

    ajeitando um bug

--- frontend/src/Components/Home/Main.tsx
+++ frontend/src/Components/Home/Main.tsx
@@ -11,10 +11,10 @@ export default  class Main extends Component {
                     <div className="sm:w-1/2 sm:h-1/2 sm:inline-block w-fit h-fit">
                         <img src={imghome} alt="Umas coisas coloridas" />
                     </div>
-                    <div className="max-w-2xl bg-white py-10 px-5 m-auto w-full mt-10 sm:mt-48">
+                    <div className="max-w-2xl bg-white py-10 px-5 m-auto w-full sm:mt-48">
                         <form className="grid grid-cols-2 gap-4 max-w-xl m-auto">
                             <div className="col-span-2 lg:col-span-2">
-                                <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Seu nome..." />
+                                <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Seu nome" />
                             </div>
                             <div className="col-span-2 text-center">
                                 <button type="submit" className="py-3 px-6 bg-gray-400 text-white font-bold w-full sm:w-full">
